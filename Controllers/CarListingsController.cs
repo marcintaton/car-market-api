2fcd68845ecfae0032b6bb18efe41b77eb176114
Marcin Tatoń <tatonmarcinpr@gmail.com>
2020-11-24 20:17:01 +0100

    Generated controllers

---
+++ Controllers/CarListingsController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CarMarket.Models;
+
+namespace CarMarket.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarListingsController : ControllerBase
+    {
+        private readonly CarMarketDBContext _context;
+
+        public CarListingsController(CarMarketDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/CarListings
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CarListing>>> GetCarListings()
+        {
+            return await _context.CarListings.ToListAsync();
+        }
+
+        // GET: api/CarListings/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<CarListing>> GetCarListing(int id)
+        {
+            var carListing = await _context.CarListings.FindAsync(id);
+
+            if (carListing == null)
+            {
+                return NotFound();
+            }
+
+            return carListing;
+        }
+
+        // PUT: api/CarListings/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCarListing(int id, CarListing carListing)
+        {
+            carListing.Id = id;
+
+            _context.Entry(carListing).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CarListingExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/CarListings
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<CarListing>> PostCarListing(CarListing carListing)
+        {
+            _context.CarListings.Add(carListing);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCarListing", new { id = carListing.Id }, carListing);
+        }
+
+        // DELETE: api/CarListings/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<CarListing>> DeleteCarListing(int id)
+        {
+            var carListing = await _context.CarListings.FindAsync(id);
+            if (carListing == null)
+            {
+                return NotFound();
+            }
+
+            _context.CarListings.Remove(carListing);
+            await _context.SaveChangesAsync();
+
+            return carListing;
+        }
+
+        private bool CarListingExists(int id)
+        {
+            return _context.CarListings.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Controllers/DirectMessagesController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CarMarket.Models;
+
+namespace CarMarket.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DirectMessagesController : ControllerBase
+    {
+        private readonly CarMarketDBContext _context;
+
+        public DirectMessagesController(CarMarketDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/DirectMessages
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<DirectMessage>>> GetDirectMessages()
+        {
+            return await _context.DirectMessages.ToListAsync();
+        }
+
+        // GET: api/DirectMessages/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<DirectMessage>> GetDirectMessage(int id)
+        {
+            var directMessage = await _context.DirectMessages.FindAsync(id);
+
+            if (directMessage == null)
+            {
+                return NotFound();
+            }
+
+            return directMessage;
+        }
+
+        // PUT: api/DirectMessages/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutDirectMessage(int id, DirectMessage directMessage)
+        {
+            if (id != directMessage.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(directMessage).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DirectMessageExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/DirectMessages
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<DirectMessage>> PostDirectMessage(DirectMessage directMessage)
+        {
+            _context.DirectMessages.Add(directMessage);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetDirectMessage", new { id = directMessage.Id }, directMessage);
+        }
+
+        // DELETE: api/DirectMessages/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<DirectMessage>> DeleteDirectMessage(int id)
+        {
+            var directMessage = await _context.DirectMessages.FindAsync(id);
+            if (directMessage == null)
+            {
+                return NotFound();
+            }
+
+            _context.DirectMessages.Remove(directMessage);
+            await _context.SaveChangesAsync();
+
+            return directMessage;
+        }
+
+        private bool DirectMessageExists(int id)
+        {
+            return _context.DirectMessages.Any(e => e.Id == id);
+        }
+    }
+}
