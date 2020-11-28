aa55a42acd2e2c945d7dad05802e7842c2a67aee
Marcin Tatoń <tatonmarcinpr@gmail.com>
2020-11-29 00:01:42 +0100

    doxygen setup

--- Models/CarListing.cs
+++ Models/CarListing.cs
@@ -8,27 +8,57 @@ using System.Threading.Tasks;
 
 namespace CarMarket.Models
 {
+    ///
+    /// Model representing a single listing object
+    ///
     public class CarListing
     {
+        ///
+        /// Unique key value
+        ///
         [Key]
         public int Id { get; set; }
 
+        ///
+        /// Precise time of listing being added, as Unix timestamp
+        ///
         public int DateAdded { get; set; }
 
+        ///
+        /// ID of the user who added the listing
+        ///
         public int UserId { get; set; }
 
+        ///
+        /// Brand of the listed car
+        ///
         [Column(TypeName = "nvarchar(20)")]
         public string Brand { set; get; }
 
+        ///
+        /// Model of the listed car
+        ///
         [Column(TypeName = "nvarchar(20)")]
         public string Model { set; get; }
 
+        ///
+        /// Production year of the listed car
+        ///
         public int ProductionYear { set; get; }
 
+        ///
+        /// Mileage of the listed car
+        ///
         public int Mileage { set; get; }
 
+        ///
+        /// Suggested price
+        ///
         public int Price { set; get; }
 
+        ///
+        /// Additional details
+        ///
         [Column(TypeName = "nvarchar(1000)")]
         public string Descrition { get; set; }
     }
